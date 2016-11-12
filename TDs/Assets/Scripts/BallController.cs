40a76d286d41d4519485c0c2b5f4c1f14e0236dd
Daphné HB <daphne.hbek@gmail.com>
2016-11-12 01:37:43 +0100

    Entrainements
    Entrainements avec les tutoriels

---
+++ TDs/Assets/Scripts/BallController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+	public int speed = 2;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		KeyboardMovements ();
+	}
+
+	void KeyboardMovements () {
+		if (Input.GetKey ("right")) {
+			rb.AddForce (Vector3.right * speed);
+		} else if (Input.GetKey ("left")) {
+			rb.AddForce (Vector3.left * speed);
+		} else if (Input.GetKey ("up")) {
+			rb.AddForce (Vector3.forward * speed);
+		} else if (Input.GetKey ("down")) {
+			rb.AddForce (Vector3.back * speed);
+		}
+	}
+}
