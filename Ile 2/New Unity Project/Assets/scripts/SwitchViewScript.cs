8f8ddfa2a831b1c4f732104236806c44e6774c63
Daphné HB <daphne.hbek@gmail.com>
2017-01-06 18:53:52 +0100

    Ile2 contenant le projet qui sera final avec la 2eme ile, un debut de menu, et 2 cameras

---
+++ Ile 2/New Unity Project/Assets/scripts/SwitchViewScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchViewScript : MonoBehaviour {
+
+	public Camera cam1;
+	public Camera cam2;
+	bool cam;
+
+
+	// Use this for initialization
+	void Start () {
+		cam = true;
+		cam1.enabled = true;
+		cam2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("v")) {
+//			cam = !cam;
+			cam1.enabled =!cam1.enabled;
+			cam2.enabled =!cam2.enabled;
+		}
+	}
+}
